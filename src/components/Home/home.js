a587990f931a4b43ed46a35c663c300bea0cb501
timothyalton <timothy.alton@gmail.com>
2022-10-16 13:19:16 -0500

    created AnimatedLetters componenent to handle animations on home page.

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,14 +1,29 @@
 import './home.scss';
 import LogoTitle from '../../assets/images/3dm.png';
 import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters/animatedLetter';
+
+import { useState } from 'react';
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['i', 't', 'c', 'h', 'e', 'l', 'l', ' ', 'a', 'l', 't', 'o', 'n']
+  const jobArray = ['s', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'e', 'n', 'g', 'i', 'n', 'e', 'e', 'r']
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>Hello, <br /> I'm
+        <span className={letterClass}>H</span>
+        <span className={`${letterClass} _12`}>i,</span>
+        <br />
+        <span className={letterClass}>I</span>
+        <span className={letterClass}>'m</span>
         <img src={LogoTitle} alt="developer" />
-        itchell Alton
+        <AnimatedLetters letterClass={letterClass}
+        strArray={nameArray}
+        idx={15}
+        />
         <br />
         Software Engineer
         </h1>
